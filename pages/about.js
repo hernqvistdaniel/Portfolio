01a5920c049612113cc302a8241c955ecb6dd292
Daniel Hernqvist <daniel.hernqvist@chasacademy.se>
2019-04-06 16:11:18 +0200

    Add Card-Form to the portfolio-page and add styling to it.

--- pages/about.js
+++ pages/about.js
@@ -6,8 +6,7 @@ class About extends React.Component {
   render() {
     return (
       <BaseLayout {...this.props.auth}>
-        <BasePage className="about-page">
-          <h1>about!</h1>
+        <BasePage className="about-page" title="About Page">
         </BasePage>
       </BaseLayout>
     )
